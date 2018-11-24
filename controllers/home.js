68c9772024cb8d2af9691cab24dfa0edfdd0dd8f
Alex esch <eschmassage@gmail.com>
2018-11-24 14:28:10 -0500

    trouble with apiroutes

--- controllers/home.js
+++ controllers/home.js
@@ -5,3 +5,10 @@ module.exports = {
     });
   }
 };
+module.exports = {
+  renderlinkedIn: function(req, res) {
+    res.render("linkedin", {
+      msg: "Welcome! this is data being passed into the render method!"
+    });
+  }
+};
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,5 +1,17 @@
 var homeController = require("../controllers/home");
-
+var linkedinController = require("../controllers/linkedin");
+var libraryController = require("../controllers/library");
+var playerController = require("../controllers/player");
 module.exports = function(app) {
   app.get("/", homeController.renderHome);
 };
+// module.exports = function(app) {
+//   app.get("/linkedin", linkedinController.renderlinkedIn);
+// };
+// module.exports = function(app) {
+//   app.get("/library", libraryController.renderLibrary);
+// };
+// module.exports = function(app) {
+//   app.get("/player", playerController.renderplayer);
+// };
+
