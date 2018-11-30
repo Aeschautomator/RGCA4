097b0c61ae0fa0f1732148ae679f8cf25ee6000e
Alex esch <eschmassage@gmail.com>
2018-11-29 19:44:36 -0500

    added authcontroller to htmlroutes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -2,6 +2,7 @@ var homeController = require("../controllers/home");
 var linkedinController = require("../controllers/linkedin");
 var libraryController = require("../controllers/library");
 var playerController = require("../controllers/player");
+var authController = require("../controllers/authcontroller")
 module.exports = function(app) {
   app.get("/index", homeController.renderHome);
 
@@ -11,6 +12,6 @@ module.exports = function(app) {
 
   app.get("/player", playerController.renderplayer);
 
-  app.get('/', authController.signin);
+  app.get('/', authController.rendersignin);
 };
 
